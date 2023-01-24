41967edc2aeb94a7bc4470f9def01fef926c8437
ubisage <obaiduzair822@gmail.com>
2023-01-24 11:45:08 +0530

    useeffect fetched menu instead hard coded dummy meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,71 @@
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
+import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
+  const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState()
+
+  useEffect(()=>{
+    const fetchMeals = async()=>{
+      
+    const response =await fetch('https://react-b6104-default-rtdb.firebaseio.com/meals.json')
+    
+    if(!response.ok){
+      throw new Error ('Something went wrong')
+    }
+
+    const responseData = await response.json()
+      // data recieved is object =nested
+
+      const loadedMeals = [];
+
+      for (const key in responseData ){
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+
+        })
+      }
+      setMeals(loadedMeals)
+      setIsLoading(false)
+  }
+// failed as fetch meals is a async function it returns a promise ,if we throw an error inside of a promise that error will cause that promise to reject
+// can use await on fetchMEals() to solve that but have to use async at useEffect which is not allowed can tackle it by having another function for tryand catch
+// try{
+//   fetchMeals();
+// }
+// catch (error){
+//   setIsLoading(false)
+//   setHttpError(error.message)
+// }
+
+fetchMeals.catch(error =>{
+     setIsLoading(false)
+     setHttpError(error.message)
+});
+
+  }, [])
+
+  if (isLoading) {
+    return <section className={classes.MealsLoading}>
+      <p>Loading ...</p>
+    </section>
+  }
+
+  if (httpError){
+    return <section className={classes.MealsError}>
+    <p>{httpError}</p>
+  </section>
+  }
+
+  const mealsList = meals.map((meal) => {
     return (
       <MealItem
         id={meal.id}
